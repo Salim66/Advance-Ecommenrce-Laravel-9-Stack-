af8b60f30b6cc111ef2fc1fbcb6306ac3095d31b
Salim66 <salimhasanriad8@gmail.com>
2022-06-08 22:56:37 +0600

    Admin Template successfully run

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->group(function () {
+    // Admin all route here
+    Route::get('/dashboard', [AdminController::class, 'dashboard']);
+});
