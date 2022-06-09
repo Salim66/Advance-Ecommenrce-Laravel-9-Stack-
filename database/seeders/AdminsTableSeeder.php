77f53177a37ba1bb3a7ad25e823a2a3db1c5a2f1
Salim66 <salimhasanriad8@gmail.com>
2022-06-09 14:27:21 +0600

    Admin seeder added

---
+++ database/seeders/AdminsTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class AdminsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->delete();
+
+        $adminRecord = [
+            'id' => 1,
+            'name' => 'admin',
+            'type' => 'admin',
+            'mobile' => '01773980593',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'), //password
+            'image' => '',
+            'status' => 1
+        ];
+
+        Admin::create($adminRecord);
+        // foreach ($adminRecord as $key => $record) {
+        // }
+    }
+}
