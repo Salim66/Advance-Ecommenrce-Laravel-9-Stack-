2bf56e58cbae924e8aef72aaf2858ebcaaa9f29d
Salim66 <salimhasanriad8@gmail.com>
2022-07-19 10:53:22 +0600

    Make Front Header Menu Dynamic | Sections/Categories/Sub-Categories

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -11,6 +11,10 @@ class Section extends Model
 
     protected $guarded = [];
 
+    public static function section(){
+        $get_sections = Section::with('categories')->where('status', 1)->get();
+        return $get_sections;
+    }
 
     public function categories(){
         return $this->hasMany(Category::class, 'section_id')->where(['parent_id' => 'ROOT', 'status' => 1])->with('subCategories');
