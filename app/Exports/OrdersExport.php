5788ca0a0eabbe5e3cca93d14c1f2636c6cc8281
Salim66 <salimhasanriad8@gmail.com>
2022-08-18 12:14:23 +0600

    Export Orders in Laravel (II) | Maatwebsite Laravel

--- app/Exports/OrdersExport.php
+++ app/Exports/OrdersExport.php
@@ -3,6 +3,7 @@
 namespace App\Exports;
 
 use App\Models\Order;
+use App\Models\OrdersProduct;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 
@@ -14,11 +15,38 @@ class OrdersExport implements FromCollection, WithHeadings
     public function collection()
     {
         $ordersData = Order::select('id','user_id', 'name', 'address', 'city', 'state', 'country', 'mobile', 'email', 'order_status', 'payment_method', 'payment_geteway', 'grand_total')->orderBy('id', 'DESC')->get();
+
+        foreach($ordersData as $key => $value){
+            // echo $value['id']; echo "<br/>";
+            $ordeItems = OrdersProduct::select('id', 'product_code', 'product_name', 'product_color', 'product_size', 'product_price', 'product_qty')->where('order_id', $value['id'])->get();
+            // $ordeItems = json_decode(json_encode($ordeItems));
+            // echo "<pre>"; print_r($ordeItems); die;
+            $product_codes = "";
+            $product_names = "";
+            $product_colors = "";
+            $product_sizes = "";
+            $product_prices = "";
+            $product_quantities = "";
+            foreach($ordeItems as $item){
+                $product_codes .= $item['product_code'] . ",";
+                $product_names .= $item['product_name'] . ",";
+                $product_colors .= $item['product_color'] . ",";
+                $product_sizes .= $item['product_size'] . ",";
+                $product_prices .= $item['product_price'] . ",";
+                $product_quantities .= $item['product_qty'] . ",";
+            }
+            $ordersData[$key]['product_codes'] = $product_codes;
+            $ordersData[$key]['product_names'] = $product_names;
+            $ordersData[$key]['product_colors'] = $product_colors;
+            $ordersData[$key]['product_sizes'] = $product_sizes;
+            $ordersData[$key]['product_prices'] = $product_prices;
+            $ordersData[$key]['product_quantities'] = $product_quantities;
+        }
         return $ordersData;
     }
 
     public function headings(): array{
-        return ['Id', 'User_ID', 'Name', 'Address', 'City', 'State', 'Country', 'Mobile', 'Email', 'Order Status', 'Payment Method', 'payment Gateway', 'Grand Total'];
+        return ['Id', 'User_ID', 'Name', 'Address', 'City', 'State', 'Country', 'Mobile', 'Email', 'Order Status', 'Payment Method', 'payment Gateway', 'Grand Total', 'Product Codes', 'product Names', 'product Colors', 'product Sizes', 'product Prices', 'product Quantities'];
     }
 
 
